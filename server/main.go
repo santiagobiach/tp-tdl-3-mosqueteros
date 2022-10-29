ab198257782c2e5d5ae01472810609a192823930
santiagobiach <santi.biach12@gmail.com>
2022-10-29 12:08:57 -0300

    Primeros pasos MongoDB

--- server/main.go
+++ server/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
 	"net"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"server/server_utils"
 )
@@ -50,6 +57,26 @@ func main() {
 		return
 	}
 	defer l.Close()
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://mongo:aRWy6fJLk5T7UlzJ@cluster0.qiccyzh.mongodb.net/?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
 
 	for {
 		c, err := l.Accept()
